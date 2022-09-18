b9a77fd1d69b3be46bd53f4c9e3b09834525969e
nicolas andrade <nicolasandrade328@gmail.com>
2022-09-18 12:55:11 -0300

    agregamos la configuracion del PUERTO

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 import express from 'express';
-console.log('hola mundo');
+
+//instancia de express
+const app = express();
+
+//queremos tomar el puerto de la pc
+app.set('port', process.env.PORT || 4000);
+
+//quiero que mi backend escuche el puerto
+app.listen(app.get('port'), () => {
+    console.log('Estamos en el puerto ' + app.get('port'));
+});
